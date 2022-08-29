bef7cb4e9bcd2a00932bab4c5dc12388ac126193
spangen87 <rikard.spangmyr@bestlas.se>
2022-08-29 12:05:58 +0000

    Finish exception modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,7 @@ async function postForm(e) {
         if (response.ok) {
             displayErrors(data);
         } else {
+            displayException(data);
             throw new Error(data.error);
         }
     }
@@ -75,6 +76,7 @@ async function postForm(e) {
             if (response.ok) {
                 displayStatus(data);
             } else {
+                displayException(data);
                 throw new Error(data.error);
             }
         }
@@ -83,6 +85,19 @@ async function postForm(e) {
             let heading = "API Key Status";
             let results = `<div>Your key is valid until</div>`;
             results += `<div class="key-status">${data.expiry}</div>`;
+
+            document.getElementById("resultsModalTitle").innerText = heading;
+            document.getElementById("results-content").innerHTML = results;
+
+            resultsModal.show();
+        }
+
+        function displayException(data) {
+            let heading = "An Exception Occured";
+
+            results = `<div>The API returned status code ${data.status_code}</div>`;
+            results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+            results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
             document.getElementById("resultsModalTitle").innerText = heading;
             document.getElementById("results-content").innerHTML = results;
